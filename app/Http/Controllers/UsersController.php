a369bda63a944d81ce73e7bb25c32895c27a9627
严丹婵 <386000643@qq.com>
2018-09-19 15:11:38 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     //注册
     public function create(){
     	return view('users.create');
+    }
+
+    public function show(User $user){
+    	return view('users.show',compact('user'));#将用户对象 $user 通过 compact 方法转化为一个关联数组
+    }
+
+    public function store(Request $request){
+    	//对提交的信息进行验证
+    	//validate 方法接收两个参数，第一个参数为用户的输入数据，第二个参数为该输入数据的验证规则。
+    	$this->validate($request,[
+    		'name'=>'required|max:50',
+    		'email'=>'required|email|unique:users|max:255',
+    		'password'=>'required|confirmed|min:6'
+    	]);
+    	$user=User::create([
+    		'name'=>$request->name,
+    		'email'=>$request->email,
+    		'password'=>bcrypt($request->password),
+    	]);
+
+    	session()->flash('success','欢迎，您将在这里开启一段新的旅程～');
+    	return redirect()->route('users.show',[$user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 Route::get('/signup','UsersController@create')->name('signup');
+Route::resource('users','UsersController'); #第一个参数为资源名称，第二个参数为控制器名称。
+
